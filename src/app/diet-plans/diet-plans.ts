00a30874884d4dff42d227a260f44a488518a3c5
Nandana-GHO <nandana@gho.care>
2026-02-26 12:00:23 +0530

    fixed api call of edit medical record

--- src/app/diet-plans/diet-plans.ts
+++ src/app/diet-plans/diet-plans.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Meals } from './meals/meals';
+import { DietGraph } from './dailyDiet/diet-graph/diet-graph';
+import { Graph } from '../dash/graph/graph';
+
 
 @Component({
   selector: 'app-diet-plans',
-  imports: [Meals],
+  imports: [Meals,DietGraph,Graph],
   templateUrl: './diet-plans.html',
   styleUrl: './diet-plans.css',
 })
